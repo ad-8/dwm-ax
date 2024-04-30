39d3ee730201c56ca2e19d2c1d58151d02243947
arthur <arthurfleck8@proton.me>
2024-04-30 16:37:45 +0200

    update font size and tag names

--- config.h
+++ config.h
@@ -10,8 +10,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Hack Nerd Font:size=10" };
-static const char dmenufont[]       = "Hack Nerd Font:size=10";
+static const char *fonts[]          = { "Hack Nerd Font:size=9" };
+static const char dmenufont[]       = "Hack Nerd Font:size=9";
 static const char col_gray1[]       = "#2e3440"; // nord polar1
 static const char col_gray2[]       = "#434c5e"; // nord polar3
 static const char col_gray3[]       = "#d8dee9"; // nord snow1
@@ -24,7 +24,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1 ", "2 ", "3", "4", "5", "6", "7", "8 ", "9 " };
+static const char *tags[] = { "", "", "3", "4", "5", "6", "7", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
