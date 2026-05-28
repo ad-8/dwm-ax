82504bef297268b4eb49567c147e8c9b6c687ce4
arthur <arthurfleck8@proton.me>
2026-05-28 08:32:49 +0200

    decrease font size

--- config.h
+++ config.h
@@ -10,8 +10,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Hack Nerd Font:size=9" };
-static const char dmenufont[]       = "Hack Nerd Font:size=9";
+static const char *fonts[]          = { "Hack Nerd Font:size=5" };
+static const char dmenufont[]       = "Hack Nerd Font:size=5";
 static const char col_gray1[]       = "#0c1014"; // gotham base0 (black)
 static const char col_gray2[]       = "#245361"; // gotham base4 (green - greyish)
 static const char col_gray3[]       = "#99d1ce"; // gotham base6 (whice - greenish)
